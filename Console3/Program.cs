244251c44089bf8e3d72b773221da5cec7e71260
AleksandraM <musatova.aleksandra@mail.ru>
2022-09-01 15:49:41 +0300

    нахождение индекса числа

--- Console3/Program.cs
+++ Console3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {31, 26, 23, 93, 64, 53, 65, 27, 81, 93};
+
+int n = array.Length;
+int find = 93;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
